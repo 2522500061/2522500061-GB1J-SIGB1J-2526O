217fb43111f28c128cfbc330ed00f9ef0c2eea5c
2522500061 <2522500061@mahasiswa.atmaluhur.ac.id>
2025-11-08 22:42:42 +0700

    menambahkan if dan endif pada form yang menghubungi

--- pertemuan-07/index.php
+++ pertemuan-07/index.php
@@ -101,10 +101,19 @@ endif;
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
 
+        <?php if (!empty($akunama)): ?>
+        <?php if (!empaty($akuemail)): ?>
+        <?php if (!empty($akupesan)): ?>
+
           <h2>Siapa Kamu?, Yang menghubungi kami!</h2>
         <p><strong>Nama :</strong><?php echo $akunama; ?> </p>
         <p><strong>Email :</strong><?php echo $akuemail; ?></p>
         <p><strong>Pesan :</strong><?php echo $akupesan; ?></p>
+
+      <?php endif; ?>
+      <?php endif; ?>
+      <?php endif; ?>
+      
       </form>
     </section>
   </main>
