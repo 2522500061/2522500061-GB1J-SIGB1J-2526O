edf533011ad67be7f7450f4d4bd30ac691eeafe1
2522500061 <2522500061@mahasiswa.atmaluhur.ac.id>
2025-11-08 21:38:53 +0700

    mengambil variabel form dengan txt

--- pertemuan-07/proses.php
+++ pertemuan-07/proses.php
@@ -1,8 +1,16 @@
-<?php 
-echo $_POST ["txtNama"];
-echo $_POST ["txtEmail"];
-echo $_POST ["txtPesan"];
-?>
+<?php
+session_start ();
 
+$akunama = $_POST ["txtNama"];
+$akuemail = $_POST ["txtEmail"];
+$akupesan = $_POST ["TxtPesan"];
+
+$_SESSION["txtNama"] =$akunama;
+$_SESSION["txtEmail"] = $akuemail;
+$_SESSION["txtPesan"] = $akupesan;
+
+
+header("Location: index.php");
+  
 
     
