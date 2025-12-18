e51cdbbb9ed20a7178cf61c5d611b17c16e4ca8c
2522500061 <2522500061@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:20:54 +0700

    menambahkan kode cid di-casting di file read.php

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -1,1 +1,14 @@
-# pertemuan-12
+# pertemuan-12 NIM   : 2522500061 <br>
+NAMA  : Savana Destiani <br>
+Kelompok : GB1J-SI <br>
+
+
+Hari ini, KAMIS 18 Desember  2025, saya belajar :<br>
+
+<ol>
+  <li>mencopy file pertemuan-11 ke folder pertemuan-12</li>
+  <li>menambahkan kode cid di-casting di file read.php</li>
+   
+ 
+<ol>
+
---
+++ pertemuan-12/localhost (1).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 15, 2025 at 06:30 PM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_pwd2025`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int NOT NULL,
+  `cnama` varchar(100) DEFAULT NULL,
+  `cemail` varchar(100) DEFAULT NULL,
+  `cpesan` text,
+  `dcreated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`, `dcreated_at`) VALUES
+(1, 'Savana Destiani', 'savanadestiani0@gmail.com ', 'hallo gess, hari ini saya menggunakan phpMyAdmin. ', '2025-12-16 01:04:18'),
+(2, 'Septia Rahmadona', 'septirahma1@gmail.com', 'hari ini kami praktek di lab gizi ', '2025-12-16 01:04:18'),
+(3, 'adelia putri', 'adeliaputrii12@gmail.com', 'hari ini kami belajar farmasetika dasar dan perhitungan dosis obat', '2025-12-16 01:04:18'),
+(4, 'septia', 'savanadestiani0@gmail.com', 'hallo ges nama aku septia temen savna', '2025-12-16 01:04:18');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
