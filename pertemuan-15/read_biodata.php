fd7d87a10dfb746ccbd252f0e977f534a303dbb5
2522500061 <2522500061@mahasiswa.atmaluhur.ac.id>
2026-01-13 13:20:14 +0700

    update file

--- pertemuan-15/read_biodata.php
+++ pertemuan-15/read_biodata.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   require_once 'koneksi.php';
   require_once 'fungsi.php';
 
@@ -52,7 +53,7 @@
       <td><?= $i++ ?></td>
       <td>
         <a href="edit_biodata.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
-        <a onclick="return confirm('Hapus <?= htmlspecialchars($row['bnama_lengkap']); ?>?')" href="proses_delete_biodata.php?id=<?= (int)$row['id']; ?>">Delete</a>
+        <a onclick="return confirm('Hapus <?= htmlspecialchars($row['cnama_lengkap']); ?>?')" href="proses_delete_biodata.php?id=<?= (int)$row['id']; ?>">Delete</a>
       </td>
       <td><?= $row['cid']; ?></td>
       <td><?= htmlspecialchars($row['cnim']); ?></td>
